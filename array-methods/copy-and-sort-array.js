8130af909afece14de9d622c56b69a29d152578d
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2025-11-18 14:13:29 -0600

    Add copy-and-sort-array

---
+++ array-methods/copy-and-sort-array.js
@@ -0,0 +1,23 @@
+/*
+  We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+
+  Create a function copySorted(arr) that returns such a copy.
+
+  let arr = ["HTML", "JavaScript", "CSS"];
+
+  let sorted = copySorted(arr);
+
+  alert( sorted ); // CSS, HTML, JavaScript
+  alert( arr ); // HTML, JavaScript, CSS (no changes)
+*/
+
+function copySorted(arr) {
+  return arr.slice().sort((a, b) => a.localeCompare(b));
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log(sorted);
+console.log(arr);
