af29dbcd02c198cf398ca9ed88502255b15d2526
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2025-11-18 22:14:26 -0600

    Add filter-unique-array-members

---
+++ array-methods/filter-unique-array-members.js
@@ -0,0 +1,43 @@
+/*
+  Let arr be an array.
+
+  Create a function unique(arr) that should return an array with unique items of arr.
+
+  For instance:
+
+  function unique(arr) {
+    // your code
+  }
+
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+
+  alert( unique(strings) ); // Hare, Krishna, :-O
+*/
+
+function unique(arr) {
+  const uniqueArr = [];
+
+  arr.forEach((element) => {
+    if (!uniqueArr.includes(element)) {
+      uniqueArr.push(element);
+    }
+  });
+
+  return uniqueArr;
+}
+
+let strings = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+console.log(unique(strings));
